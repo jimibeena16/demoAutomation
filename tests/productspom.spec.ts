4b24ad31a52f00061b4385e99067bc6e90d5987d
jimibeena16 <jimibeena16@gmail.com>
2026-09-17 21:24:17 +0530

    Update productspom.spec.ts

--- tests/productspom.spec.ts
+++ tests/productspom.spec.ts
@@ -25,37 +25,12 @@ test('Add Backpack to Cart', async ({ page }) => {
   await productsPage.openCart();
 
   // Verify cart
-  await expect(cartPage.productNames).toHaveText('Sauce Labs Backpack');
+  await expect(cartPage.productName).toHaveText('Sauce Labs Backpack');
 });
 
 
-test('Add multiple products to cart', async ({ page }) => {
 
-  const loginPage = new LoginPage(page);
-  const productsPage = new ProductsPage(page);
-const cartPage = new CartPage(page);
-
-  // Login
-  await loginPage.open();
-
-  await loginPage.login('standard_user','secret_sauce');
-
-  // Verify Products page
-  await expect(productsPage.pageTitle).toHaveText('Products');
-
-  // Add first product
-  await productsPage.addProductToCart('sauce-labs-backpack');
-
-  // Add second product
-  await productsPage.addProductToCart('sauce-labs-bike-light');
-
-  // Add third product
-  await productsPage.addProductToCart('sauce-labs-bolt-t-shirt');
-
-  // Go to cart
-  await productsPage.openCart();
-
-  // Verify products
+ 
   await expect(cartPage.productNames).toHaveText([
       'Sauce Labs Backpack',
       'Sauce Labs Bike Light',
