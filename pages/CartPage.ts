2e9c9148021dd04f3fce0105878e4abc56cfce13
jimibeena16 <jimibeena16@gmail.com>
2026-09-17 21:23:12 +0530

    Update CartPage.ts

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -4,14 +4,14 @@ export class CartPage {
 
   readonly page: Page;
 
-  readonly productNames: Locator;
+  readonly productName: Locator;
   readonly checkoutButton: Locator;
   readonly pageTitle: Locator;
 
   constructor(page: Page) {
     this.page = page;
 
-    this.productNames = page.locator('.inventory_item_name');
+    this.productName = page.locator('.inventory_item_name');
     this.checkoutButton = page.locator('#checkout');
     this.pageTitle = page.locator('.title');
   }
