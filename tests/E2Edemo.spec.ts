b53ea22b86f8e8a2f9a71ecc3539be0122d49e39
jimibeena16 <jimibeena16@gmail.com>
2026-09-18 11:48:46 +0530

    Update E2Edemo.spec.ts

--- tests/E2Edemo.spec.ts
+++ tests/E2Edemo.spec.ts
@@ -6,7 +6,7 @@ test('SauceDemo End to End Flow', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 
   // Login
-  await page.locator('#user-name').fill('standard_user');
+  await page.locator('#user-name').fill('standard_user')
   await page.locator('#password').fill('secret_sauce');
   await page.locator('#login-button').click();
 
